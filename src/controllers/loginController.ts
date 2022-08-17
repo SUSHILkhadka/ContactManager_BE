9c3f491fadb59de7d61df9953e43eab25f547486
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-08-17 19:18:26 +0545

    fourth try

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import CustomError from '../middlewares/CustomError';
-import * as LoginService from '../services/LoginService';
+import * as LoginService from '../services/loginService';
 
 /**
  * 
