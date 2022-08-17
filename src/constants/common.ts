0a610205d56fb85bec20b9aa6187b912445e9943
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-08-17 21:41:33 +0545

    change refresh token expiry time

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 export const SALT_ROUND = 10;
 export const EXPIRY_TIME = '4s';
-export const EXPIRY_TIME_REFRESH_TOKEN = 1000*300;
+export const EXPIRY_TIME_REFRESH_TOKEN = 1000*1000*59;// 59 mins
--- src/db/db.ts
+++ src/db/db.ts
@@ -2,3 +2,6 @@ import Knex from 'knex';
 import knexConfig from './knexfile';
 const knex = Knex(knexConfig.production);
 export default knex;
+
+
+// heroku config:set PGSSLMODE=no-verify
