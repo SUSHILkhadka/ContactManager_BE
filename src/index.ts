0eeb8da63594b75e64496b717d5bf6d39329cebd
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-08-18 08:04:59 +0545

    add send status

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import cors from 'cors';
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
+app.use(cors({origin: /\.herokuapp\.com$/}));
 app.options('/upload',(req:Request,res:Response)=>{
 res.sendStatus(204);
 })
