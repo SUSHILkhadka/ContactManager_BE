06765abc8ea57a72a50e99ffe26944d9e31d33b1
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-08-18 07:58:05 +0545

    add send status

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ app.options('/upload',(req:Request,res:Response)=>{
 res.sendStatus(204);
 })
 
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.all('*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
 
 // app.enableCors({
 //   origin: true,
