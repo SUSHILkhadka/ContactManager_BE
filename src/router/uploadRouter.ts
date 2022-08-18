67082ee43f45bb522170c8905366c9627c86e64a
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-08-18 09:47:45 +0545

    fully working even with authentication middleare for upload

--- src/router/uploadRouter.ts
+++ src/router/uploadRouter.ts
@@ -4,7 +4,7 @@ import CustomError from '../middlewares/CustomError';
 import cloudinary from '../config/cloudinary';
 import logger from '../misc/Logger';
 import upload from '../config/multer';
-
+import fs from "fs"
 const router = Router();
 
 /**
@@ -34,11 +34,10 @@ async function uploadFiles(req: any, res: Response, next: NextFunction) {
       upload_preset: 'contacts-photo',
     });
     logger.info('successfully uploaded image to cloudinary');
-    console.log('url = ', uploadResponse.url);
-    // fs.unlinkSync(filePath);
+    fs.unlinkSync(filePath);
     return res.json({ url: uploadResponse.url });
   } catch (e) {
-    // fs.unlinkSync(req.files[0].path);
+    fs.unlinkSync(req.files[0].path);
     logger.error('upload failed');
     return next(new CustomError(`${e}`, StatusCodes.INTERNAL_SERVER_ERROR));
   }
