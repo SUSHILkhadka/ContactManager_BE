67bdc37df8e9ac98d941764fe979bb332d9cee73
SUSHILkhadka <shusilkhadka2@gmail.com>
2022-09-18 20:03:36 +0545

    change multer config and uploadfiles

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,20 +1,35 @@
-import multer from 'multer';
+import { Request } from "express";
+import multer from "multer";
+import logger from "../misc/Logger";
 
+type DestinationCallback = (error: Error | any, destination: string) => void;
+type FileNameCallback = (error: Error | any, filename: string) => void;
 //local disk storage configuration for multer
 const storage = multer.diskStorage({
-  destination: function (req: any, file: any, cb: any) {
-    if (file) {
-    console.log(file)
-      cb(null, `src/assets/uploads`);
-    } else {
-      cb('multer error');
+  destination: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: DestinationCallback
+  ) {
+    try {
+      if (file) cb(null, `src/assets/uploads`);
+      logger.info("setting up destination for file");
+    } catch (e: Error | any) {
+      logger.error("setting up destination for file,failed");
+      cb(e, "multer error");
     }
   },
-  filename: function (req: any, file: any, cb: any) {
-    if (file) {
-      cb(null, Date.now() + '_' + file.originalname);
-    } else {
-      cb('multer error');
+  filename: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: FileNameCallback
+  ) {
+    try {
+      if (file) cb(null, Date.now() + "_" + file.originalname);
+      logger.info("setting up filename for file");
+    } catch (e: Error | any) {
+      logger.info("setting up filename for file,failed");
+      cb(e, "multer error");
     }
   },
 });
